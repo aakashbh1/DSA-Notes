9481a98cd001a79a768dfa618be8b23bb8b3f9b6
Aakash Bhargava <92159539+aakashbh1@users.noreply.github.com>
2023-09-18 13:12:42 +0530

    Create Top View.cpp

---
+++ Trees/Traversals & Views of Tree/Top View.cpp
@@ -0,0 +1,43 @@
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    vector<int> topView(Node *root)
+    {
+        //Your code here
+        
+        vector<int> ans;
+        if(root==NULL){
+            return ans;
+            
+        }
+        
+        map<int,int> m;
+        queue<pair<Node*,int> > q;
+        q.push(make_pair(root,0));
+        
+        while(!q.empty()){
+            pair<Node*,int> temp = q.front();
+            q.pop();
+            Node* frontNode = temp.first;
+            int hd = temp.second;
+            
+            
+            if(m.find(hd)==m.end()){
+                m[hd]=frontNode->data;
+            }
+            if(frontNode->left){
+                q.push(make_pair(frontNode->left,hd-1));
+            }
+            
+            if(frontNode->right){
+                q.push(make_pair(frontNode->right,hd+1));
+            }
+        }
+        for(auto i:m){
+            ans.push_back(i.second);
+        }
+    }
+
+};
