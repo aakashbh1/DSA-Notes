e5bb88702b4af0c23c0a6a659a55780291491d2f
Aakash Bhargava <92159539+aakashbh1@users.noreply.github.com>
2023-10-05 16:50:55 +0530

    Update Moore's Voting Algorithm.cpp

--- IMPORTANT ALGO'S/Moore's Voting Algorithm.cpp
+++ IMPORTANT ALGO'S/Moore's Voting Algorithm.cpp
@@ -7,6 +7,7 @@ You may assume that the majority element always exists in the array.
 // Moore's majority vote algorithm
 // https://en.wikipedia.org/wiki/Boyer–Moore_majority_vote_algorithm
 // Assumption: the array is non-empty and the majority element always exist in the array
+// occurs n/2
 int majorityElement(vector<int>& nums) {
     int candidate = nums[0], counter = 0;
     for (int n: nums) {
@@ -21,3 +22,50 @@ int majorityElement(vector<int>& nums) {
     }
     return candidate;
 }
+
+//occurs n/3
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int num1 = INT_MIN, num2 = INT_MIN;
+        int count1 = 0, count2 = 0;
+        for(auto element : nums){
+            if(num1 == element){
+                count1++;
+            }
+            else if(num2 == element){
+                count2++;
+            }
+            else if(count1 == 0){
+                num1 = element;
+                count1 = 1;
+            }
+            else if(count2 == 0){
+                num2 = element;
+                count2 = 1;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+        }
+        vector<int> output;
+        int countMajority = nums.size()/3;
+        count1 = 0, count2 = 0;
+        for(auto element : nums){
+            if(num1 == element){
+                count1++;
+            }
+            if(num2 == element){
+                count2++;
+            }
+        }
+        if(count1 > countMajority){
+            output.push_back(num1);
+        }
+        if(count2 > countMajority){
+            output.push_back(num2);
+        }
+        return output;
+    }
+};
